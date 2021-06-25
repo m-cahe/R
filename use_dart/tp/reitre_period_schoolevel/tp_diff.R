1e3d79056ef74c412682e66d9300ff5d4a30c994
moon <gaon4336@naver.com>
2021-06-25 12:27:20 +0900

    grouping ing... -msg

--- use_dart/tp/reitre_period_schoolevel/tp_diff.R
+++ use_dart/tp/reitre_period_schoolevel/tp_diff.R
@@ -17,7 +17,13 @@ install.packages('dplyr')
 library(googleVis)
 library(dplyr)
 
-df %>% filter('재직기간'==1)
+View(df)
 
 # %>% : chain operation (%>%), pipe operation   //ctrl + shift + M
 # 오류 could not find function "%>%"  에 대한 대안 -> packages('dplyr')설치&libray(dplyr) 
+
+#특정 열출력 df$열이름
+data.frame(df$재직기간)
+
+#1.재직기간의 첫번째 글자를 추출해서 행에 추가
+#2.추가한 부분을 기준으로 groupby 해서 sum()
